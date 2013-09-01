7f264b698b809dc73636aca81a988d914d530e78
Jose Antonio Gonzalez Pereira <jgpereira23@gmail.com>
2013-09-01 11:27:47 -0400

    Font Change/ Footer
    Changed font from Verdana to Century Gothic. Added footer: Contact Us,
    General Info

--- SHPE New/jscript.js
+++ SHPE New/jscript.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
-
 	$("#navbar ul li").hover(function(){
 		$(this).children(".submenu").fadeToggle("fast");
 	});
 	
-	$("#logo").click(function(){//change to thumb class for every video use
+	$(".thumb").click(function(){//change to thumb class for every video use
 		$("#inactive").css("z-index", "4");
 		$("#inactive").fadeTo('slow', 0.5, function(){
 			$("#vid").fadeIn();
-			$("iframe").attr("src", $("iframe").attr("src").replace("autoplay=0", "autoplay=1"));//change to <a> tag using player
+			//$("iframe").attr("src", $("iframe").attr("src").replace("autoplay=0", "autoplay=1"));//change to <a> tag using player
 		});
 	});
 	$("#inactive").click(function(){
 		$("#vid").fadeOut(function(){
+			$("#inactive").fadeTo('slow',0.0)
 			$("#inactive").css("z-index", "-1");
-			$("#inactive").fadeTo('slow', 0.0);
-			$('iframe').get(0).stopVideo();
 		});
 	});
 });
