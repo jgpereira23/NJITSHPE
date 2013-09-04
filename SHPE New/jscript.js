a86c2fa76b5cf1054c4f94494c34a2db012d09b3
Jose Antonio Gonzalez Pereira <jgpereira23@gmail.com>
2013-09-04 01:03:16 -0400

    Live Version
    Added page information

--- SHPE New/jscript.js
+++ SHPE New/jscript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$("#navbar ul li").hover(function(){
 		$(this).children(".submenu").fadeToggle("fast");
 	});
-	
+	$("#UpdateTxt").html("Our website got a fresh look. Explore every nook and cranny of it!");
 	$("#logo").click(function(){
 		$("#inactive").css("z-index", "4");
 		$("#inactive").fadeTo('slow', 0.5, function(){
@@ -21,24 +21,18 @@ $(document).ready(function(){
 		$("#UpImg1").css("border-color","#0B3383");
 		$("#UpImg2").css("border-color","black");
 		$("#UpImg3").css("border-color","black");
-		$("#Update1").show();
-		$("#Update2").hide();
-		$("#Update3").hide();
+		$("#UpdateTxt").html("Our website got a fresh look. Explore every nook and cranny of it!");
 	});
 	$("#UpImg2").hover(function() {
 		$("#UpImg1").css("border-color","black");
 		$("#UpImg2").css("border-color","#0B3383");
 		$("#UpImg3").css("border-color","black");
-		$("#Update1").hide();
-		$("#Update2").show();
-		$("#Update3").hide();
+		$("#UpdateTxt").html("Chechout the calendar to see what NJIT SHPE will be doing next.");
 	});
 	$("#UpImg3").hover(function() {
 		$("#UpImg1").css("border-color","black");
 		$("#UpImg2").css("border-color","black");
 		$("#UpImg3").css("border-color","#0B3383");
-		$("#Update1").hide();
-		$("#Update2").hide();
-		$("#Update3").show();
+		$("#UpdateTxt").html("Click on our social links to take part in our community!");
 	});
 });
