13d795f9bde0a04bd948db89b7dc9b2872b1a730
Mario DeLaPaz <mariocd10@gmail.com>
2014-01-19 02:17:24 -0500

    Nested while loop
    I loop through album and then loop through each photo to display it.
    There are still bugs. Doesn't display more than one album.

--- Scripts/displayPhotos.php
+++ Scripts/displayPhotos.php
@@ -1,42 +1,54 @@
 <?php
 require("connect.php");
-
-$link = 
-
-$query = "SELECT link FROM album;";
-$maxCol = 3;
-$result = mysql_query($query);
-
-echo "<div id='right'>";
-//Open Table
-echo"<table cellpadding='5' cols='2'>";
-//set index var to track record count
-$recIdx=0;
-while($row = mysql_fetch_array($result)){
-	$recIdx++;
-	//Open new row if needed
-	if($recIdx%$maxCol ==1){
-		echo "<tr>\n";
+//get data from both tables
+$query1 = "SELECT * FROM album;";
+$query2 = "SELECT * from pictures;";
+//storing data in var
+$result1 = mysql_query($query1);
+$result2 = mysql_query($query2);
+//setting album count and column count
+$aCount = 0;
+$colMax = 3;
+//opening a table
+echo "<table cellpadding='5' cols='2'>";
+//loop through the album records
+while($aRow = mysql_fetch_array($result1)){
+	$aCount++;
+	$firstPic=true;
+	//make a new row if needed
+	if($aCount% $colMax ==1){
+			echo "<tr>\n";
+		}
+	//loop through the pictures records for each album
+	while($pRow = mysql_fetch_array($result2)){
+		$k = $pRow['albumID'];
+		//if album id equals pictures id display photos
+		if($aRow['albumID'] == $k){
+			echo "<tr>";
+			//if its the first photo then display with picture
+			if($firstPic == true){
+				echo "<td>";
+				echo "<a href='{$aRow['link']}/{$pRow['link']}' data-lightbox='{$aRow['name']}' title='{$pRow['caption']}'><img src='{$aRow['link']}/{$pRow['link']}' alt='thumb-1' width='150' height='150'></br>{$aRow['name']} </a>";
+				echo "</td>\n";
+				$firstPic = false;
+			}
+			echo "<td>";
+			echo "<a href='{$aRow['link']}/{$pRow['link']}' data-lightbox='{$aRow['name']}' title='{$pRow['caption']}'></a>";
+			echo "<td>\n";
+		}
 	}
-	//Display Album
-	
-	echo "<td>";
-	echo "<img src='Images\Gallery\albumcover.png' width='100px' height='100px'></br>";
-	echo $row['name'];
-	echo "</td>\n";
 	//Close row if needed
-	if($recIdx % $maxCol ==0){
+	if($aCount % $colMax ==0){
 		echo "</tr>\n";
 	}
 }
-//Close last row if needed
-if($recIdx % $maxCol == 0){
-	echo "</tr>\n";
-}
-//close table and div
-echo "</table>";
-echo "</div>";
-					
 
+//Close row if needed
+if($aCount % $colMax ==0){
+	echo "</tr>\n";
+	}
+	
+//close table
+echo "</table>";
 
 ?>
