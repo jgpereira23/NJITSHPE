5d86cc6cd8515f2624b3f82a4f03cadee28f3ea9
Mario DeLaPaz <mariocd10@gmail.com>
2014-01-26 22:47:57 -0500

    edits

--- Scripts/registerUser.php
+++ Scripts/registerUser.php
@@ -4,7 +4,7 @@
 	$sql = "INSERT INTO user VALUES ('', '$_POST[firstName]', '$_POST[lastName]', '$_POST[email]', '$_POST[email]', '$_POST[major]', '$_POST[gradDate]', '$_POST[gender]','$_POST[number]', '$_POST[dateOfBirth]','$_POST[middleName]', '$_POST[ethnicity]', '$_POST[legalStatus]','$_POST[address]','$_POST[city]','$_POST[state]','$_POST[country]','$_POST[postalCode]','$_POST[degree]','$_POST[gpa]','$_POST[gradeLevel]','','$_POST[password]')";
 	
 	if(!mysql_query($sql)){
-		dte("Error: "  , mysql_error());
+		echo "Error: "  , mysql_error();
 	}
 	include('uploadResume.php');
 	header("location:../index.php");
