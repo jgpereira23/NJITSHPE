97f128578bdb0a97709bfd98c03d92b2ede25219
Mario DeLaPaz <mariocd10@gmail.com>
2014-01-26 21:56:20 -0500

    connect

--- Scripts/connect.php
+++ Scripts/connect.php
@@ -1,12 +1,8 @@
+<?php
+//MYSQL Connection
+$link = mysql_connect('localhost','root','america1') or die("Unable to Connect to MYsQL");
 
-<?php	
-$host="sql2.njit.edu"; // Host name 
-$username="shpe_club"; // Mysql username 
-$password="2in91IH0I2Kz8Shd9V"; // Mysql password 
-$db_name="shpe_club"; // Database name 
-
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("Cannot Connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
-$selected = mysql_select_db($dbname, $dbh);
+//Selecting a DB
+$dbname = 'shpenjit';
+mysql_select_db($dbname , $link);
 ?>
